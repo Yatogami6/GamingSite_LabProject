c440c0aac8d3d9a3ee9bac1d246e0095b0372a90
Yato <94577042+Yatogami6@users.noreply.github.com>
2023-10-29 17:44:36 +0000

    Update dark-mode.js

--- dark-mode.js
+++ dark-mode.js
@@ -1,18 +1,17 @@
-<script>
-  const themeToggle = document.getElementById("theme-toggle");
-  const lightStylesheet = document.getElementById("light-stylesheet");
-  const darkStylesheet = document.getElementById("dark-stylesheet");
-  let isDarkMode = false;
+const themeToggle = document.getElementById("theme-toggle");
+const lightStylesheet = document.getElementById("light-stylesheet");
+const darkStylesheet = document.getElementById("dark-stylesheet");
+let isDarkMode = false;
 
-  themeToggle.addEventListener("click", () => {
-    if (isDarkMode) {
-      lightStylesheet.disabled = false;
-      darkStylesheet.disabled = true;
-    } else {
-      lightStylesheet.disabled = true;
-      darkStylesheet.disabled = false;
-    }
+themeToggle.addEventListener("click", () => {
+  if (isDarkMode) {
+    lightStylesheet.disabled = false;
+    darkStylesheet.disabled = true;
+  } else {
+    lightStylesheet.disabled = true;
+    darkStylesheet.disabled = false;
+  }
 
-    isDarkMode = !isDarkMode;
-  });
-</script>
+  isDarkMode = !isDarkMode;
+});
+
